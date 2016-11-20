89d411357ed6e55546eef39a2b2ee610833adca6
Sergei O. Udalov <sergei.udalov@gmail.com>
2016-11-20 15:28:05 +0300

    sample strategy in progress

--- my_strategy.rb
+++ my_strategy.rb
@@ -4,14 +4,82 @@ require './model/move'
 require './model/world'
 
 class MyStrategy
+  def initialize
+    @waypoints_by_lane = {}
+  end
+
   # @param [Wizard] me
   # @param [World] world
   # @param [Game] game
   # @param [Move] move
   def move(me, world, game, move)
+    initialize_strategy(me, game)
+
     move.speed = game.wizard_forward_speed
     move.strafe_speed = game.wizard_strafe_speed
     move.turn = game.wizard_max_turn_angle
     move.action = ActionType::MAGIC_MISSILE
+  end
+
+  private
+
+  def initialize_strategy(me, game)
+    if @random == nil
+      @random = Random.new(game.random_seed)
+
+      map_size = game.map_size
+
+      @waypoints_by_lane[LaneType::MIDDLE] = [
+        Point2D.new(100, map_size - 100),
+        random_bool ? Point2D.new(600, map_size - 200) : Point2D.new(200, map_size - 600),
+        Point2D.new(800, map_size - 800),
+        Point2D.new(map_size - 600, 600),
+      ]
+
+      @waypoints_by_lane[LaneType::TOP] = [
+        Point2D.new(100, map_size - 100),
+        Point2D.new(100, map_size - 400),
+        Point2D.new(200, map_size - 800),
+        Point2D.new(200, map_size * 0.75),
+        Point2D.new(200, map_size * 0.5),
+        Point2D.new(200, map_size * 0.25),
+        Point2D.new(200, 200),
+        Point2D.new(map_size * 0.25, 200),
+        Point2D.new(map_size * 0.5, 200),
+        Point2D.new(map_size * 0.75, 200),
+        Point2D.new(map_size - 200, 200),
+      ]
+
+      @waypoints_by_lane[LaneType::BOTTOM] = [
+        Point2D.new(100, map_size - 100),
+        Point2D.new(400, map_size - 100),
+        Point2D.new(800, map_size - 200),
+        Point2D.new(map_size * 0.25, map_size - 200),
+        Point2D.new(map_size * 0.5, map_size - 200),
+        Point2D.new(map_size * 0.75, map_size - 200),
+        Point2D.new(map_size - 200, map_size - 200),
+        Point2D.new(map_size - 200, map_size * 0.75),
+        Point2D.new(map_size - 200, map_size * 0.5),
+        Point2D.new(map_size - 200, map_size * 0.25),
+        Point2D.new(map_size - 200, 200),
+      ]
+    else
+    end
+  end
+
+  def random_bool
+    @random.rand > 0.5
+  end
+
+  class Point2D
+    attr_reader :x, :y
+
+    def initialize(x, y)
+      @x, @y = x, y
+    end
+
+    def get_distance_to(point)
+      (point.x - x) ** 2 + (point.y - y) ** 2
+    end
   end
 end
