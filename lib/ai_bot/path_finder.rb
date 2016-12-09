51f9b1e35a79b6be531bd5373e6e57ce3b93c58e
Sergei O. Udalov <sergei.udalov@gmail.com>
2016-12-09 16:09:49 +0300

    wave path find

--- lib/ai_bot/path_finder.rb
+++ lib/ai_bot/path_finder.rb
@@ -5,7 +5,40 @@ module AiBot
     end
 
     def find(from:, to:)
-      [from, to]
+      d = 0
+      marks = {}
+      marks[from] = 0
+
+      next_nodes = @grid.neighbors_for(from)
+
+      until marks.keys.include?(to)
+        d += 1
+
+        next_nodes.each do |node|
+          marks[node] = d
+        end
+
+        next_nodes = next_nodes.map { |node| @grid.neighbors_for(node) }.flatten - marks.keys
+        break if next_nodes.empty?
+      end
+
+      return [] unless marks.keys.include?(to)
+
+      inversed_path = [to]
+
+      current = to
+
+      until current == from
+        next_node = @grid.neighbors_for(current).find do |node|
+          marks[node] == marks[current] - 1
+        end
+
+        inversed_path << next_node
+
+        current = next_node
+      end
+
+      inversed_path.reverse!
     end
   end
 end
--- spec/ai_bot/path_finder_spec.rb
+++ spec/ai_bot/path_finder_spec.rb
@@ -2,38 +2,47 @@ require 'spec_helper'
 
 class Grid
   def initialize
-    @nodes = []
+    @nodes = {}
   end
 
   def node_at(x, y)
-    @nodes.dig(x, y)
+    @nodes.dig(x.to_i, y.to_i)
   end
 
-  def add_node(x, y)
-    @nodes[x] ||= []
-    @nodes[x][y] = AiBot::Point.new(x, y)
+  def add_node(node)
+    @nodes[node.x.to_i] ||= {}
+    @nodes[node.x.to_i][node.y.to_i] = node
+  end
+
+  def neighbors_for(node)
+    [
+      node_at(node.x - 1, node.y),
+      node_at(node.x, node.y - 1),
+      node_at(node.x + 1, node.y),
+      node_at(node.x, node.y + 1)
+    ].compact
   end
 end
 
 def build_grid(input)
-
   new_grid = Grid.new
 
   input.split("\n").each_with_index do |line, y|
     line.split.each_with_index do |node, x|
-      new_grid.add_node(x, y) if node == '0'
+      new_grid.add_node AiBot::Point.new(x, y) if node == '0'
     end
   end
 
+  new_grid
 end
 
 describe AiBot::PathFinder do
   let(:path_finder) { AiBot::PathFinder.new grid: grid }
 
   describe 'found path' do
-    subject { path_finder.find(from: from_point, to: to_point).map { |p| [p.x, p.y] } }
-    let(:from_point) { AiBot::Point.new(from[0], from[1]) }
-    let(:to_point) { AiBot::Point.new(to[0], to[1]) }
+    subject { path_finder.find(from: from_point, to: to_point).map { |node| [node.x, node.y] } }
+    let(:from_point) { grid.node_at(*from) }
+    let(:to_point) { grid.node_at(*to) }
 
     context 'when map is empty' do
       let(:grid) do
@@ -44,10 +53,25 @@ describe AiBot::PathFinder do
         GRID
       end
 
-      let(:from) { [1, 1] }
-      let(:to) { [1, 2] }
+      let(:from) { [0, 1] }
+      let(:to) { [2, 1] }
 
-      it { is_expected.to eq [[1, 1], [1, 2]]}
+      it { is_expected.to eq [[0, 1], [1, 1], [2, 1]] }
+    end
+
+    context do
+      let(:grid) do
+        build_grid <<-GRID
+          0 1 0
+          0 1 0
+          0 0 0
+        GRID
+      end
+
+      let(:from) { [0, 1] }
+      let(:to) { [2, 1] }
+
+      it { is_expected.to eq [[0, 1], [0, 2], [1, 2], [2, 2], [2, 1]] }
     end
   end
 end
